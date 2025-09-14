07b69a29d6b0a3e5742f25ac4f174adbd9059855
artikmay@mail.ru <artikmay@mail.ru>
2025-09-14 16:49:28 +0500

    setup a project

--- src/main/java/org/weatherApp/config/SpringConfig.java
+++ src/main/java/org/weatherApp/config/SpringConfig.java
@@ -1,10 +1,27 @@
 package org.weatherApp.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
+@ComponentScan("org.weatherApp")
 @EnableWebMvc
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final ThymeleafViewResolver thymeleafViewResolver;
+
+    public SpringConfig(ThymeleafViewResolver thymeleafViewResolver) {
+        this.thymeleafViewResolver = thymeleafViewResolver;
+    }
+
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(thymeleafViewResolver);
+    }
 
 }
---
+++ src/main/java/org/weatherApp/config/ThymeleafConfig.java
@@ -0,0 +1,43 @@
+package org.weatherApp.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    private final ApplicationContext applicationContext;
+
+    public ThymeleafConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+}
